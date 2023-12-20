3c6ebb19e65b2b897755c16f0b5cb077eb9978b6
Simra <simratabassum789@gmail.com>
2023-12-20 17:23:22 -0500

    Program to print the series: 1,2,factor of three,4,5,factor of three, ...........,22,23,factor of three.

---
+++ src/main/scala/Program27.scala
@@ -0,0 +1,12 @@
+object Program27 {
+  def main(args:Array[String]):Unit={
+    for(num<- 1 to 24){
+      if(num%3==0){
+        print("factor of three, ")
+      }
+      else{
+        print(num+", ")
+      }
+    }
+  }
+}
