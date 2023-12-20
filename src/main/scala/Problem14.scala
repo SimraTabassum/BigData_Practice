505ceb2df5283053342f65394a16a4a85070fe75
Simra <simratabassum789@gmail.com>
2023-12-20 16:16:13 -0500

    Program to print alternate even numbers from 20 to 140.(22,26,30,.....)

---
+++ src/main/scala/Problem14.scala
@@ -0,0 +1,13 @@
+object Problem14 {
+  def main(args:Array[String]):Unit = {
+    var a=1
+    for(num<- 20 to 140){
+      if(num%2==0){
+        if(a%2==0){
+          println(num)
+        }
+        a=a+1
+      }
+    }
+  }
+}
