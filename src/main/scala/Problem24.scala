40b1f09608c2b2f27f06ca5ec2ac89c18fc49264
Simra <simratabassum789@gmail.com>
2023-12-20 17:19:16 -0500

    Program to print the series: 5,10,5,10,5,10,5

---
+++ src/main/scala/Problem24.scala
@@ -0,0 +1,12 @@
+object Problem24 {
+ def main(args:Array[String]):Unit={
+   for(num<- 1 to 7){
+     if(num%2!=0){
+       print("5, ")
+     }
+     else{
+       print("10, ")
+     }
+   }
+ }
+}
