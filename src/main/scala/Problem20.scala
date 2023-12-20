743dfbfb14b12a9d66578aa99e95f75c01f0becf
Simra <simratabassum789@gmail.com>
2023-12-20 16:26:17 -0500

    Program to print A,B alternatively for 100 times.

---
+++ src/main/scala/Problem20.scala
@@ -0,0 +1,12 @@
+object Problem20 {
+  def main(args: Array[String]): Unit = {
+    for(i<- 1 to 100){
+      if(i%2==0){
+        print("B, ")
+      }
+      else{
+        print("A, ")
+      }
+    }
+  }
+}
