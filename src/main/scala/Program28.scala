37c0b6cfba9b336c8bd737dd861d0776947522c8
Simra <simratabassum789@gmail.com>
2023-12-20 17:25:20 -0500

    Program to print the series: 1,3,divisible by five, 7,9,11,13,divisible by five, ............, 21,23,divisible by five,

---
+++ src/main/scala/Program28.scala
@@ -0,0 +1,17 @@
+object Program28 {
+ def main(args:Array[String]):Unit={
+   for(num<- 1 to 25){
+     if(num%2==0){
+
+     }
+     else{
+       if(num%5==0){
+         print("divisble by five, ")
+       }
+       else{
+         print(num+", ")
+       }
+     }
+   }
+ }
+}
